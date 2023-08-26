757f7c51f8c232952bd9e4995deb22452a1a3091
Alx-Lai <b09902135@csie.ntu.edu.tw>
2023-08-26 09:45:00 +0800

    update default code

--- 01_Basic/default_code.cpp
+++ 01_Basic/default_code.cpp
@@ -3,9 +3,9 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
+#define debug(x) {cerr<<#x<<" = "<<x<<'\n';}
 #define X first
 #define Y second
-#define SZ(a) ((int)a.size())
 #define pb push_back
 signed main() {
   ios::sync_with_stdio(0);
