a512bf573378ad00690c6a674868cfddca61f1f0
Ming <shimeming@gmail.com>
2023-08-31 21:35:04 +0800

    Add the usage of erase in black_magic

--- 01_Basic/black_magic.cpp
+++ 01_Basic/black_magic.cpp
@@ -5,6 +5,8 @@ using namespace __gnu_pbds;
 using namespace std;
 #define ll long long
 typedef __gnu_pbds::priority_queue<int> heap;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 int main() {
   heap h1, h2;
   h1.push(1), h1.push(3);
@@ -17,5 +19,6 @@ int main() {
   for (int x : {0, 2, 3, 4})
     st.insert(x);
   cout << *st.find_by_order(2) << st.order_of_key(1) << endl; // 31
+  // erase: st.erase(s.find_by_order(s.order_of_key(v)));
 }
 //__int128_t,__float128_t
